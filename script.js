a94b1b7aa83aa669fc82008e9c77fc71a95b13c6
medinaotniel <medotniel@gmail.com>
2022-05-10 23:13:27 -0400

    Create CPU RPS function

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
+// 0 == rock
+// 1 == paper
+// 2 == scissors
+
 console.log("test");
-let userGame;
-let cpuGame;
+let userValue;
+let cpuValue;
 // User is prompted with welcome message and asked to play RPS
-userValue();
+userGetValue();
 //User then inputs RPS
 //The user RPS value is then stored in a variable and a function
+//User RPS is sent to be converted from string to numerical value
 //is called to get CPU RPS
-function userValue() {
-  userGame = prompt(
+function userGetValue() {
+  userValue = prompt(
     "Welcome to RPS. Type 'rock', 'paper', or 'scissors' to play."
   );
-  cpuValue();
+  cpuGetValue();
+  convert();
 }
 
+//converts user RPS to numerical value
 //CPU RPS is stored in variable
+function cpuGetValue() {
+  cpuValue = Math.floor(Math.random() * 3);
+}
 //CPU RPS and user RPS is then sent to be evaluated to see if there is a tie or win/loss
 //The result is then stored and sent to another function that displays the result
 //A function will receive the result and displays the appropriate result message
