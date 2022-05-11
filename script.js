ff818528731273a3a3a7ef6de2210dbb918e5dcd
medinaotniel <medotniel@gmail.com>
2022-05-11 00:36:58 -0400

    Create function that evalueates game result and other that displays it

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 console.log("test");
 let userValue;
 let cpuValue;
+let gameResult;
 // User is prompted with welcome message and asked to play RPS
 userGetValue();
 //User then inputs RPS
@@ -16,7 +17,6 @@ function userGetValue() {
     "Welcome to RPS. Type 'rock', 'paper', or 'scissors' to play."
   );
   convert(userValue);
-  cpuGetValue();
 }
 
 //converts user RPS to numerical value
@@ -24,12 +24,43 @@ function convert(value) {
   if (value === "rock") userValue = 0;
   else if (value === "paper") userValue = 1;
   else userValue = 2;
+  cpuGetValue();
+  getResult();
 }
 //CPU RPS is stored in variable
 function cpuGetValue() {
   cpuValue = Math.floor(Math.random() * 3);
+  console.log("Test");
 }
 //CPU RPS and user RPS is then sent to be evaluated to see if there is a tie or win/loss
+function getResult() {
+  switch (true) {
+    case userValue === cpuValue:
+      gameResult = null;
+      break;
+    case userValue === 0 && cpuValue === 2:
+      gameResult = true;
+      break;
+    case userValue === 1 && cpuValue === 0:
+      gameResult = true;
+      break;
+    case userValue === 2 && cpuValue === 1:
+      gameResult = true;
+      break;
+    default:
+      gameResult = false;
+      break;
+  }
+  displayResult();
+}
 //The result is then stored and sent to another function that displays the result
 //A function will receive the result and displays the appropriate result message
 //The game is now over
+function displayResult() {
+  if (gameResult == true) console.log("You Win!");
+  else if (gameResult == false) console.log("You Lose! HAHAHA");
+  else console.log("Its a tie... boring");
+}
+//function that controls stack
+//control();
+function control() {}
